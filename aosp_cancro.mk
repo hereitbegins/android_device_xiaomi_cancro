70f4fcd316e602a10c755ae49ceac8be27a32e59
hereitbegins <hereitbegins18@gmail.com>
2016-09-04 14:38:39 +0800

    Update aosp_cancro.mk

--- aosp_cancro.mk
+++ aosp_cancro.mk
@@ -19,10 +19,16 @@ TARGET_SCREEN_WIDTH := 1080
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
-PRODUCT_NAME := aosp_cancro
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := cm_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
